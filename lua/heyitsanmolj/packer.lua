8e170c2376f2570823b552fe80cc687c038a7794
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-01 17:25:02 +0530

    added nvim-treesitter

--- lua/heyitsanmolj/packer.lua
+++ lua/heyitsanmolj/packer.lua
@@ -8,4 +8,5 @@ return require('packer').startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+	use 'nvim-treesitter/nvim-treesitter'
 end)
