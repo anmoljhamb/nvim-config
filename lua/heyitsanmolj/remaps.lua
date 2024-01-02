80fe61178f285fec47c7ca26aba04e6d60d8e312
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-02 08:46:18 +0530

    added a shortcut for alt backspace

--- lua/heyitsanmolj/remaps.lua
+++ lua/heyitsanmolj/remaps.lua
@@ -24,7 +24,7 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
-
+vim.keymap.set("i", "<A-BS>", "<C-w>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
