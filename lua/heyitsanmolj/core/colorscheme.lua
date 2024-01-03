b498ee2755c41ff0d3482aad7a053b44f556ffd4
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-03 21:19:33 +0530

    lsp setup complete

--- lua/heyitsanmolj/core/colorscheme.lua
+++ lua/heyitsanmolj/core/colorscheme.lua
@@ -2,8 +2,8 @@ function setScheme(color)
     color = color or "desert"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
+    vim.api.nvim_set_hl(50, "Normal", { bg = "None" })
+    vim.api.nvim_set_hl(50, "NormalFloat", { bg = "None" })
 end
 
 setScheme("github_dark")
