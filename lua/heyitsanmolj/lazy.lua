a97b437e286d4247b7ee46bcc58632e5f07fce08
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-03 14:18:21 +0530

    refactor - lazy.lua

--- lua/heyitsanmolj/lazy.lua
+++ lua/heyitsanmolj/lazy.lua
@@ -11,4 +11,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("heyitsanmolj.plugins")
+require("lazy").setup(
+{
+    { import = "heyitsanmolj.plugins" }
+}, 
+{
+    checker = {
+        enabled = true,
+        notify = false
+    },
+    change_detection = {
+        notify = false
+    }
+}
+)
