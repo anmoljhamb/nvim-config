5dfac725ba70419647660e9fc787726239d99c80
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-04 09:27:51 +0530

    added the keymaping for deleting words using C-BS and A-BS, and to toggle comments using C-/ and A-/

--- lua/heyitsanmolj/core/keymaps.lua
+++ lua/heyitsanmolj/core/keymaps.lua
@@ -1,41 +1,54 @@
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
+local km = vim.keymap
 
 ---------------------
 -- General Keymaps
 ---------------------
 
+---------------------
+-- Insert Keybindings
+---------------------
+
 -- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>")
+km.set("i", "jk", "<ESC>")
+km.set("i", "<A-BS>", "<C-W>")
+km.set("i", "<C-H>", "<C-W>")
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+km.set("n", "<leader>nh", ":nohl<CR>")
 
 -- delete single character without copying into register
-keymap.set("n", "x", '"_x')
+km.set("n", "x", '"_x')
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>") -- increment
-keymap.set("n", "<leader>-", "<C-x>") -- decrement
+km.set("n", "<leader>+", "<C-a>") -- increment
+km.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+km.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+km.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+km.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+km.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tw", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+km.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+km.set("n", "<leader>tw", ":tabclose<CR>") -- close current tab
+km.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+km.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 -- plugin keymaps
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+km.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- NvimTreeToggle
-keymap.set("n", "<A-b>", ":NvimTreeToggle<CR>")
+km.set("n", "<A-b>", ":NvimTreeToggle<CR>")
 
-keymap.set("n", "<A-p>", ":Telescope git_files<CR>")
-keymap.set("n", "<leader>pf", ":Telescope find_files<CR>")
-keymap.set("n", "<leader>ps", ":Telescope live_grep<CR>")
+km.set("n", "<A-p>", ":Telescope git_files<CR>")
+km.set("n", "<leader>pf", ":Telescope find_files<CR>")
+km.set("n", "<leader>ps", ":Telescope live_grep<CR>")
+
+-- Comments
+km.set("n", "<C-_>", "gcc", { remap = true })
+km.set("v", "<C-_>", "gc", { remap = true })
+km.set("n", "<A-/>", "gcc", { remap = true })
+km.set("v", "<A-/>", "gc", { remap = true })
+km.set("i", "<C-_>", "<Esc>gccA ", { remap = true })
--- lua/heyitsanmolj/plugins/comments.lua
+++ lua/heyitsanmolj/plugins/comments.lua
@@ -1,8 +1,7 @@
 return {
-    'numToStr/Comment.nvim',
-    lazy = false,
-    config = function()
-        require("Comment").setup()
-    end
+	"numToStr/Comment.nvim",
+	lazy = false,
+	config = function()
+		require("Comment").setup()
+	end,
 }
-
