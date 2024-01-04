606f3c91e72ad9f076e55507c1acc44899256ed4
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-04 10:40:26 +0530

    refactor, formatted

--- lua/heyitsanmolj/core/colorscheme.lua
+++ lua/heyitsanmolj/core/colorscheme.lua
@@ -1,9 +1,9 @@
 function setScheme(color)
-    color = color or "desert"
-    vim.cmd.colorscheme(color)
+	color = color or "desert"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(50, "Normal", { bg = "None" })
-    vim.api.nvim_set_hl(50, "NormalFloat", { bg = "None" })
+	vim.api.nvim_set_hl(50, "Normal", { bg = "None" })
+	vim.api.nvim_set_hl(50, "NormalFloat", { bg = "None" })
 end
 
 setScheme("github_dark")
