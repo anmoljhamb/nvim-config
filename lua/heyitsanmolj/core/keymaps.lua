fcc97dc664c40944e610e9d67af6a63459ec4ade
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-04 10:15:48 +0530

    Added a keybinding to close windows using <A-w>

--- lua/heyitsanmolj/core/keymaps.lua
+++ lua/heyitsanmolj/core/keymaps.lua
@@ -50,6 +50,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+km.set("n", "<A-w>", ":q!<CR>")
 
 ---------------------
 -- Visual Keybindings
