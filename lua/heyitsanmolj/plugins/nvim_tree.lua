277511d6b027353663a79f3a28a851d953eefc8b
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-25 17:23:18 +0530

    modified the arrow icons

--- lua/heyitsanmolj/plugins/nvim_tree.lua
+++ lua/heyitsanmolj/plugins/nvim_tree.lua
@@ -1,35 +1,35 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-        -- disable netrw at the very start of your init.lua
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		-- disable netrw at the very start of your init.lua
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        -- set termguicolors to enable highlight groups
-        vim.opt.termguicolors = true
+		-- set termguicolors to enable highlight groups
+		vim.opt.termguicolors = true
 
-        -- empty setup using defaults
-        require("nvim-tree").setup({
-            renderer = {
-                icons = {
-                    glyphs = {
-                        folder = {
-                            arrow_closed = "", -- arrow when folder is closed
-                            arrow_open = "", -- arrow when folder is open
-                        },
-                    },
-                },
-            },
-            -- disable window_picker for
-            -- explorer to work well with
-            -- window splits
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false,
-                    },
-                },
-            },
-        })
-    end
+		-- empty setup using defaults
+		require("nvim-tree").setup({
+			renderer = {
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			-- disable window_picker for
+			-- explorer to work well with
+			-- window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+		})
+	end,
 }
