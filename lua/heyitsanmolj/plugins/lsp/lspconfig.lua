c745e054a0b8d08f859931b2a73ffb36215ea036
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-28 09:45:57 +0530

    Added the lsp for dart.

--- lua/heyitsanmolj/plugins/lsp/lspconfig.lua
+++ lua/heyitsanmolj/plugins/lsp/lspconfig.lua
@@ -200,5 +200,40 @@ return {
 				},
 			},
 		})
+
+		local dartExcludedFolders = {
+			-- vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+			-- vim.fn.expand("$HOME/.pub-cache"),
+			-- vim.fn.expand("/opt/homebrew/"),
+			-- vim.fn.expand("$HOME/tools/flutter/"),
+		}
+
+		lspconfig["dartls"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			cmd = {
+				"dart",
+				"language-server",
+				"--protocol=lsp",
+				-- "--port=8123",
+				-- "--instrumentation-log-file=/Users/robertbrunhage/Desktop/lsp-log.txt",
+			},
+			filetypes = { "dart" },
+			init_options = {
+				onlyAnalyzeProjectsWithOpenFiles = false,
+				suggestFromUnimportedLibraries = true,
+				closingLabels = true,
+				outline = false,
+				flutterOutline = false,
+			},
+			settings = {
+				dart = {
+					analysisExcludedFolders = dartExcludedFolders,
+					updateImportsOnRename = true,
+					completeFunctionCalls = true,
+					showTodos = true,
+				},
+			},
+		})
 	end,
 }
