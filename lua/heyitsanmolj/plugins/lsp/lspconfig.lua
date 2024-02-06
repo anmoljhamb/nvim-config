87c330f3164234c7e7ac112bb347a6bb2e705bc7
Anmol Jhamb <talktoanmol@outlook.com>
2024-02-06 06:32:44 +0530

    Added one more keybinding to the code actions

--- lua/heyitsanmolj/plugins/lsp/lspconfig.lua
+++ lua/heyitsanmolj/plugins/lsp/lspconfig.lua
@@ -46,6 +46,7 @@ return {
 
 			opts.desc = "See available code actions"
 			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			keymap.set({ "n", "v" }, "<A-.>", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 			opts.desc = "Smart rename"
 			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
