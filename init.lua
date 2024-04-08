8c64b11e63cd92898982e1543f22c1ebec902ff4
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-08 22:59:06 +0530

    added support for vscode

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
 require("heyitsanmolj")
+
+local km = vim.keymap
+if vim.g.vscode then
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = ";"
+	vim.cmd("nmap <leader>c :e ~/.config/nvim/init.lua<CR>")
+	print("Loading the nvim config")
+end
