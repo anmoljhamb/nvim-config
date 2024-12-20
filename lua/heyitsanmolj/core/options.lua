2c758f48591a611e0fb841ec7caabfd2f9782fe1
Anmol Jhamb <talktoanmol@outlook.com>
2024-12-20 10:32:18 +0000

    yank to windows clipboard

--- lua/heyitsanmolj/core/options.lua
+++ lua/heyitsanmolj/core/options.lua
@@ -62,3 +62,16 @@ vim.filetype.add({
 		ejs = "html",
 	},
 })
+
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+	vim.api.nvim_create_autocmd("TextYankPost", {
+
+		group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+
+		callback = function()
+			vim.fn.system("clip.exe", vim.fn.getreg('"'))
+		end,
+	})
+end
