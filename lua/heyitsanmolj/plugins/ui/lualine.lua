588009d44454eaf9d25fb58f2f0cb4a36c462f9e
Anmol Jhamb <talktoanmol@outlook.com>
2025-01-09 07:23:13 +0530

    modified the lualine

--- lua/heyitsanmolj/plugins/ui/lualine.lua
+++ lua/heyitsanmolj/plugins/ui/lualine.lua
@@ -4,26 +4,6 @@ return {
 	config = function()
 		local local_theme = require("lualine.themes.nightfly")
 		require("lualine").setup({
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						function()
-							-- Count the number of open buffers
-							local buffers = vim.tbl_filter(function(buf)
-								return vim.api.nvim_buf_is_loaded(buf)
-							end, vim.api.nvim_list_bufs())
-							return #buffers
-						end,
-						-- Optional icon
-						icon = "", -- Or any other icon
-					},
-				},
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
 			opts = {
 				theme = local_theme,
 			},
