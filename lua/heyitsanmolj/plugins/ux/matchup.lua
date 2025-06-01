5fe44dfbc7f3d314f1357875dd1121aa71c4d554
Anmol Jhamb <talktoanmol@outlook.com>
2025-06-01 21:32:04 +0530

    make it so that i can sleect next item using f13

--- lua/heyitsanmolj/plugins/ux/matchup.lua
+++ lua/heyitsanmolj/plugins/ux/matchup.lua
@@ -1,4 +1,8 @@
 return {
 	"andymass/vim-matchup",
 	event = "BufReadPost",
+	config = function()
+		vim.g.matchup_matchparen_offscreen = {}
+		vim.g.matchup_matchparen_enabled = 0 -- Disable matchparen to avoid conflicts
+	end,
 }
--- lua/heyitsanmolj/plugins/ux/nvim-cmp.lua
+++ lua/heyitsanmolj/plugins/ux/nvim-cmp.lua
@@ -39,6 +39,7 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<A-q>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<A-e>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<F13>"] = cmp.mapping.select_next_item(), -- next suggestion
 				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
@@ -47,8 +48,8 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "luasnip" }, -- snippets
 				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 				{ name = "vim-dadbod-completion" },
